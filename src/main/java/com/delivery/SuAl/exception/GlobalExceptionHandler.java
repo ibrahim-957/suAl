47c8f69350ba663b7427f4e6c43e75f5fc201dfd
ibrahim-957 <n.naghiyev.ibrahim@gmail.com>
2026-01-13 20:35:26 +0400

    HealthController added

--- src/main/java/com/delivery/SuAl/exception/GlobalExceptionHandler.java
+++ src/main/java/com/delivery/SuAl/exception/GlobalExceptionHandler.java
@@ -7,6 +7,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.servlet.resource.NoResourceFoundException;
 
 import java.time.LocalDateTime;
 
@@ -76,6 +77,11 @@ public class GlobalExceptionHandler {
                 "Unexpected server error",
                 request
         );
+    }
+
+    @ExceptionHandler(NoResourceFoundException.class)
+    public ResponseEntity<String> handleNoResourceFound(NoResourceFoundException ex) {
+        return ResponseEntity.notFound().build();
     }
 
     private ResponseEntity<ErrorResponse> buildError(
