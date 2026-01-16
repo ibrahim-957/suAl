1be79865c42a2f83b17076f32269fc7ab095f7dd
ibrahim-957 <n.naghiyev.ibrahim@gmail.com>
2026-01-16 14:10:31 +0400

    time show problem solve

--- src/main/java/com/delivery/SuAl/config/WebConfig.java
+++ src/main/java/com/delivery/SuAl/config/WebConfig.java
@@ -1,5 +1,6 @@
 package com.delivery.SuAl.config;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.format.FormatterRegistry;
@@ -13,15 +14,24 @@ import java.util.List;
 public class WebConfig implements WebMvcConfigurer {
 
     private final StringToMapConverter stringToMapConverter;
+    private final ObjectMapper objectMapper;
 
-    public WebConfig(StringToMapConverter stringToMapConverter) {
+    public WebConfig(StringToMapConverter stringToMapConverter, ObjectMapper objectMapper) {
         this.stringToMapConverter = stringToMapConverter;
+        this.objectMapper = objectMapper;
     }
 
     @Bean
     public MappingJackson2HttpMessageConverter octetStreamConverter() {
-        MappingJackson2HttpMessageConverter converter = new MappingJackson2HttpMessageConverter();
-        converter.setSupportedMediaTypes(List.of(new MediaType("application", "octet-stream")));
+        MappingJackson2HttpMessageConverter converter = new MappingJackson2HttpMessageConverter(objectMapper);
+
+        objectMapper.registerModule(new com.fasterxml.jackson.datatype.jsr310.JavaTimeModule());
+        objectMapper.configure(com.fasterxml.jackson.databind.SerializationFeature.WRITE_DATES_AS_TIMESTAMPS, false);
+
+        converter.setSupportedMediaTypes(List.of(
+                new MediaType("application", "octet-stream"),
+                new MediaType("application", "json")
+        ));
         return converter;
     }
 
