1fc7bd9c7c168b4e486e45304ed475b1ced0dc3a
ibrahim-957 <n.naghiyev.ibrahim@gmail.com>
2026-01-30 23:06:47 +0400

    feat: flyway error

--- src/main/resources/db/migration/V9__remove_unique_email_and_phone.sql
+++ src/main/resources/db/migration/V9__remove_unique_email_and_phone.sql
@@ -1,5 +1,4 @@
-ALTER TABLE users
-    DROP CONSTRAINT IF EXISTS users_email_key;
+ALTER TABLE users DROP CONSTRAINT IF EXISTS users_email_key;
 
-ALTER TABLE users
-    DROP CONSTRAINT IF EXISTS users_phone_number_key1;
+ALTER TABLE users DROP CONSTRAINT IF EXISTS users_phone_number_key;
+ALTER TABLE users DROP CONSTRAINT IF EXISTS users_phone_number_key1;
