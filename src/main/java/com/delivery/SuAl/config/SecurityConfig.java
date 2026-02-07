0a227f7c77e6b752a5f248c2839c53a65691c49d
ibrahim-957 <n.naghiyev.ibrahim@gmail.com>
2026-02-07 12:37:08 +0400

    role

--- src/main/java/com/delivery/SuAl/config/SecurityConfig.java
+++ src/main/java/com/delivery/SuAl/config/SecurityConfig.java
@@ -61,6 +61,7 @@ public class SecurityConfig {
                         .requestMatchers("/v1/api/customers/addresses/**").hasAnyRole("ADMIN", "CUSTOMER", "OPERATOR")
 
                         .requestMatchers("/v1/api/customers/**").hasAnyRole("ADMIN", "OPERATOR", "CUSTOMER")
+                        .requestMatchers("/v1/api/drivers/orders").hasAnyRole("ADMIN", "DRIVER")
                         .requestMatchers("/v1/api/drivers/**").hasAnyRole("ADMIN", "OPERATOR")
                         .requestMatchers("/v1/api/operators/**").hasRole("ADMIN")
 
@@ -73,7 +74,6 @@ public class SecurityConfig {
 
                         .requestMatchers("/v1/api/promos/**").hasAnyRole("ADMIN", "OPERATOR")
 
-                        .requestMatchers("/v1/api/drivers/orders/**").hasAnyRole("ADMIN", "DRIVER")
                         .requestMatchers("/v1/api/orders/my-orders").hasAnyRole("ADMIN", "CUSTOMER")
                         .requestMatchers("/v1/api/orders/customer/**/reject").hasAnyRole("ADMIN", "CUSTOMER")
                         .requestMatchers("/v1/api/orders/**").hasAnyRole("ADMIN", "CUSTOMER", "OPERATOR")
